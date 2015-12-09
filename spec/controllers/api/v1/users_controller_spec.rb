abbc531b8aa9fb09040327589f47e78b25b0bb99
Luis Miguel Delgado <lmd411@gmail.com>
2015-12-09 17:56:23 -0600

    Add the destroy users endpoint

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -42,11 +42,6 @@ describe Api::V1::UsersController do
         post :create, { user: @invalid_user_attributes }, format: :json
       end
 
-      it "renders an errors json" do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response).to have_key(:errors)
-      end
-
       it "renders the json errors on why the user could not be created" do
         user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response[:errors][:email]).to include "can't be blank"
@@ -70,5 +65,14 @@ describe Api::V1::UsersController do
 
       it { should respond_with 200 }
     end
+  end
+
+  describe "DELETE #destroy" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      delete :destroy, { id: @user.id }, format: :json
+    end
+
+    it { should respond_with 204 }
   end
 end
