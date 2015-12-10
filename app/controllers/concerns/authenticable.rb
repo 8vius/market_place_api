06c184c0f3ed7195b1100a36dba965cf49bf65d8
Luis Miguel Delgado <lmd411@gmail.com>
2015-12-10 13:34:18 -0600

    Adds the authenticate with token method to handle access to actions

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -1,4 +1,9 @@
 module Authenticable
+  def authenticate_with_token!
+    render json: { errors: "Not authenticated" },
+      status: :unauthorized unless current_user.present?
+  end
+
   def current_user
     @current_user ||= User.find_by(auth_token: authorization_header)
   end
