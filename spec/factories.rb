af563f6cf2411f6f9184d884053d1a5554e35f0d
Luis Miguel Delgado <lmd411@gmail.com>
2015-12-10 16:55:29 -0600

    Adds product model bare bones along with some validations

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,12 @@
 FactoryGirl.define do
+
+  factory :product do
+    title { FFaker::Product.product_name }
+    price { rand() * 100 }
+    published false
+    user
+  end
+
   factory :user do
     email { FFaker::Internet.email }
     password "12345678"
