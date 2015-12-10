af223f1781ccc12112969ac02b88c98db4685c39
Luis Miguel Delgado <lmd411@gmail.com>
2015-12-10 12:18:38 -0600

    Refactor FactoryGirl settings. Adds user authentication token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ActiveRecord::Base
+  before_create :generate_authentication_token!
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :email, uniqueness: true
+  validates :auth_token, uniqueness: true
+
+  def generate_authentication_token!
+    begin
+      self.auth_token = Devise.friendly_token
+    end while self.class.exists?(auth_token: auth_token)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,32 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = FactoryGirl.build(:user) }
+  before { @user = build(:user) }
   subject { @user }
 
   it { should respond_to(:email) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:auth_token) }
 
   it { should validate_presence_of(:email) }
-  it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
+  it { should validate_uniqueness_of(:auth_token) }
+  it { should validate_confirmation_of(:password) }
 
   it { should be_valid }
+
+  describe "#generate_authentication_token!" do
+    it "generates a unique auth token" do
+      Devise.stub(:friendly_token).and_return("auniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).to eq "auniquetoken123"
+    end
+
+    it "generates another token when one has already been taken" do
+      existing_user = create(:user, auth_token: "auniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).to_not eq existing_user.auth_token
+    end
+  end
 end
